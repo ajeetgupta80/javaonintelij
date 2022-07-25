9de14324754aeaf5005e9b07dd28998d05d8c836
fucking ajeet <ajeet808080@gmail.com>
2022-07-25 23:15:18 +0530

    7th day only 20min

---
+++ binarysearch-2darray/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args)
+    {
+//      matrix is sorted in row and coloumn wise manner
+//        try to think how u can eliminate row and coloumn
+//        {
+//            {10,12,13},
+//            {11,14,18},
+//            {12,15,20}
+//        }
+        int[][] matrix = {
+                {10,12,13},
+                {11,14,18},
+                {12,15,20}
+
+        };
+        int target = 15;
+        System.out.println(Arrays.toString(rowcolmatrix(matrix, target)));
+
+
+    }
+    static int[] rowcolmatrix(int[][] matrix, int target)
+    {
+        int row =0;
+        int column = matrix.length-1;
+        while(row < matrix.length && column >=0)
+        {
+            if(matrix[row][column]==target)
+            {
+                return new int[]{row, column};
+            }
+            if(matrix[row][column]>target)
+            {
+                column --;
+
+            }
+            else{
+                row ++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+
+
+}
