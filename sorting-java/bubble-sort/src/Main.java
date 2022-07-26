1d24daba304168b4c0d81f82f7f2dc9e27296cba
fucking ajeet <ajeet808080@gmail.com>
2022-07-26 14:56:09 +0530

    8th day

--- sorting-java/bubble-sort/src/Main.java
+++ sorting-java/bubble-sort/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {1, 2, 3, 4, 5};
+        bubblesort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubblesort(int[] arr)
+    {
+        boolean swapped;
+        for(int i = 0; i< arr.length-1; i++)
+        {
+            swapped = false;
+            for(int j =1; j< arr.length-i; j++)
+            {
+//                swap if item is smaller than the previous one
+                if(arr[j]<arr[j-1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped)
+            {
+                break;
+            }
+        }
+
+
     }
 }
