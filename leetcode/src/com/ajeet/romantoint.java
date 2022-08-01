8a44a287b25d7b678896335fef33295ce05542a3
fucking ajeet <ajeet808080@gmail.com>
2022-08-02 00:16:05 +0530

    13th day

--- leetcode/src/com/ajeet/romantoint.java
+++ leetcode/src/com/ajeet/romantoint.java
@@ -5,30 +5,35 @@ import java.util.Map;
 
 public class romantoint {
     public static void main(String[] args) {
-        String s = "VI";
+        String s = "IX";
         int ans = romanToInt(s);
         System.out.println(ans);
 
     }
+
     static int romanToInt(String s) {
-        Map<Character,Integer> RomanVal = new HashMap<>();
-        RomanVal.put('I',1);
-        RomanVal.put('V',5);
-        RomanVal.put('X',10);
-        RomanVal.put('L',50);
-        RomanVal.put('C',100);
-        RomanVal.put('D',500);
-        RomanVal.put('M',1000);
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+        char[] arr = s.toCharArray();
+        int Length = s.length();
         int sum = 0;
-        char[] Array = s.toCharArray();
-        int Lenght = s.length();
-        for(int i = 0 ;i < Lenght - 1; i++) {
-            if(RomanVal.get(Array[i]) >= RomanVal.get(Array[i + 1])) {
-                sum+=RomanVal.get(Array[i]);
-            }else{
-                sum-=RomanVal.get(Array[i]);
+        for (int i = 0; i < Length - 1; i++) {
+            if (roman.get(arr[i]) >= roman.get(arr[i + 1])) {
+                sum += roman.get(arr[i]);
+            } else {
+                sum -= roman.get(arr[i]);
             }
+
+
         }
-        return sum+RomanVal.get(Array[Array.length - 1]);
+        return sum + roman.get(arr[arr.length - 1]);
+
+
     }
 }
