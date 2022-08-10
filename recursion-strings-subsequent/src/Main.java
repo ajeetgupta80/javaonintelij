952a42d75446185754247e3171716ecdf48603d4
fucking ajeet <ajeet808080@gmail.com>
2022-08-10 23:42:22 +0530

    22th day

--- recursion-strings-subsequent/src/Main.java
+++ recursion-strings-subsequent/src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+        int n = 36;
+        System.out.println(powoftwo(n));
     }
+    static boolean powoftwo(int n)
+    {
+        if(n==1 || n ==2)
+        {
+            return true;
+        }
+        if(n<1)
+        {
+            return false;
+        }
+        if(n%2==0)
+        {
+            return powoftwo(n/2);
+        }
+        return false;
+    }
+
 }
