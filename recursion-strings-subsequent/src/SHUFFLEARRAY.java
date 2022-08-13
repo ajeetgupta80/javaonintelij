ba5a6dd5cfec8f495df6f90085d812202efb54d4
fucking ajeet <ajeet808080@gmail.com>
2022-08-13 23:41:37 +0530

    25th day

--- recursion-strings-subsequent/src/SHUFFLEARRAY.java
+++ recursion-strings-subsequent/src/SHUFFLEARRAY.java
@@ -12,7 +12,7 @@ public class SHUFFLEARRAY {
     {
         int[] result = new int[2*n];
         for(int i =0; i<n; i++)
-        {git 
+        {
             result[2*i]= nums[i];
             result[2*i+1]= nums[n+i];
         }
--- recursion-strings-subsequent/src/subsets.java
+++ recursion-strings-subsequent/src/subsets.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class subsets {
+    // it is the pattern of taking some element and removing other element from the list or array
 
     public static void main(String[] args)
     {
-
+        subset("" , "abc");
 
 
     }
-
+    static void subset(String p, String up)
+    {
+        if(up.isEmpty())
+        {
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        subset(p+ch, up.substring(1));
+        subset(p, up.substring(1));
+    }
 
 }
