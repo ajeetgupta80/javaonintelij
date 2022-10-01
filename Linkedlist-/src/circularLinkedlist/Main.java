7fb6afb0607c6ee6f2fccee89be267324ae86e1a
fucking ajeet <ajeet808080@gmail.com>
2022-10-01 11:14:21 +0530

    linked list completed

--- Linkedlist-/src/circularLinkedlist/Main.java
+++ Linkedlist-/src/circularLinkedlist/Main.java
@@ -2,6 +2,14 @@ package circularLinkedlist;
 
 public class Main {
     public static void main(String[] args) {
-        
+       LL list = new LL();
+       list.insert(1);
+       list.insert(2);
+       list.insert(3);
+       list.insert(4);
+       list.insert(5);
+        System.out.println(list.getSize());
+        list.delete(3);
+       list.display();
     }
 }
